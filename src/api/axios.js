06c4534765ed6e20fdc7552b3a9732ee58573e41
Sreya Manoharan <sreyampk@gmail.com>
2026-06-08 12:13:11 +0530

    url

--- src/api/axios.js
+++ src/api/axios.js
@@ -39,7 +39,7 @@ import { Store } from '../store/Store';
 import axios from "axios"
 
 // const baseURL = 'http://localhost:3000/'
-const baseURL = 'https://feelhome-server-production.up.railway.app'
+const baseURL = 'https://feelhome-backend.onrender.com'
 const axiosInstance = axios.create({
   baseURL: baseURL
 })
